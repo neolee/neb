eacfcd24f94bdc9865834f1376a40aab4d5b5cf7
Neo Lee <neo.lee@gmail.com>
2025-03-07 21:00:59 +0800

    feat: pydantic model integration

---
+++ pydantic_model.py
@@ -0,0 +1,24 @@
+import os
+from typing import cast
+from pydantic import BaseModel
+
+import instrument
+
+from pydantic_ai import Agent
+from pydantic_ai.models import KnownModelName
+
+import model
+
+
+class MyModel(BaseModel):
+    city: str
+    country: str
+
+
+city_agent = Agent(model.default, result_type=MyModel)
+
+if __name__ == '__main__':
+    # result = agent.run_sync('The windy city in the USA.')
+    result = city_agent.run_sync('The capital city of PR of China.')
+    print(result.data)
+    print(result.usage())
