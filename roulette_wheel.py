596a7d53f852688d2a4284c6ab7fe77ec70286de
Neo Lee <neo.lee@gmail.com>
2025-03-07 20:10:53 +0800

    feat: agent basic usage

---
+++ roulette_wheel.py
@@ -0,0 +1,28 @@
+from pydantic_ai import Agent, RunContext
+import model
+
+
+roulette_agent = Agent(
+    model=model.default,
+    deps_type=int,
+    result_type=bool,
+    system_prompt=(
+        'Use the `roulette_wheel` function to see if the '
+        'customer has won based on the number they provide.'
+    ),
+)
+
+
+@roulette_agent.tool
+async def roulette_wheel(ctx: RunContext[int], square: int) -> str:
+    """check if the square is a winner"""
+    return 'winner' if square == ctx.deps else 'loser'
+
+
+# run the agent
+success_number = 18
+result = roulette_agent.run_sync('Put my money on square eighteen', deps=success_number)
+print(result.data)
+
+result = roulette_agent.run_sync('I bet five is the winner', deps=success_number)
+print(result.data)
