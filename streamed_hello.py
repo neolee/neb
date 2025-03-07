2e01992f8d040d85e147d201a6ac69844723f2b0
Neo Lee <neo.lee@gmail.com>
2025-03-07 19:46:48 +0800

    feat: streamed hello world

---
+++ streamed_hello.py
@@ -0,0 +1,14 @@
+import asyncio
+from pydantic_ai import Agent
+import model
+
+
+agent = Agent(model=model.default)
+
+async def main():
+    async with agent.run_stream('Where does "hello world" come from?') as result:
+        async for message in result.stream_text(delta=True):
+            print(message or "", end="", flush=True)
+
+
+asyncio.run(main())
