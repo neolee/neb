e2e9367594073bb7caa8261a426dce50f016002c
Neo Lee <neo.lee@gmail.com>
2025-07-14 02:43:01 +0800

    feat: add more logfire instruments

--- instrument.py
+++ instrument.py
@@ -4,6 +4,10 @@ from pydantic_ai import Agent
 
 def init():
     logfire.configure(send_to_logfire='if-token-present')
-    logfire.instrument_mcp()
+    logfire.instrument_system_metrics()
     logfire.instrument_asyncpg()
+    logfire.instrument_pydantic_ai()
+    logfire.instrument_pydantic()
+    logfire.instrument_mcp()
+
     Agent.instrument_all()
