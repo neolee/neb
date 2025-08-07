e9ac6480e66c7191dca2c76123ca6bcba023e5fd
Neo Lee <neo.lee@gmail.com>
2025-08-07 12:11:03 +0800

    fix: logfire scrubbing config

--- instrument.py
+++ instrument.py
@@ -1,8 +1,17 @@
 import logfire
 
 
+# more fine grained control through a callback, if needed
+def scrubbing_callback(match: logfire.ScrubMatch):
+    # `my_safe_value` often contains the string 'password' but it's not actually sensitive
+    if match.path == ('attributes', 'my_safe_value') and match.pattern_match.group(0) == 'password':
+        # return the original value to prevent redaction
+        return match.value
+
+
 def init():
-    logfire.configure()
+    # logfire.configure(scrubbing=logfire.ScrubbingOptions(callback=scrubbing_callback))
+    logfire.configure(scrubbing=False)
     logfire.instrument_pydantic_ai()
     logfire.instrument_asyncpg()
     logfire.instrument_mcp()
