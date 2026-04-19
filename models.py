33493cc4b596e8983a18c2ae47202a05cdeb6fe0
Neo Lee <neo.lee@gmail.com>
2026-04-19 16:41:53 +0800

    env: update mal and packages

--- models.py
+++ models.py
@@ -4,13 +4,11 @@ from mal.adapter.pydantic_ai import openai_model
 deepseek = openai_model("deepseek/deepseek-chat")
 deepseek_reasoner = openai_model("deepseek/deepseek-reasoner")
 
-qwen = openai_model("qwen")
+qwen = openai_model("qwen/qwen3.5-plus")
 
 gpt = openai_model("openai")
 
-openrouter_gemini_flash = openai_model("openrouter/google/gemini-3-flash-preview")
-openrouter_gemini_pro = openai_model("openrouter/google/gemini-3-pro-preview")
-
 local = openai_model("local")
+omlx = openai_model("omlx")
 
 default = qwen
